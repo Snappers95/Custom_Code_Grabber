558ee51cab769206706792e3da5972d794bd1b70
Snappers95 <laviolettejacob@gmail.com>
2024-03-18 08:39:24 -0400

    small changes, extra comments

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def get_code_title(code_snippet):
     """
     list_to_ignore = ['CREATE', 'OR', 'REPLACE', '(']
 
-    if re.search(r'^CREATE[0-9A-Z_. ()]*\n', code_snippet):
+    if re.search(r'^CREATE[0-9A-Z_. ()]*\n', code_snippet):  # Maybe change to filter out certain files
         title = re.search(r'^CREATE[0-9A-Z_. ()]*\n', code_snippet).group()
         type_name = [item for item in title.split() if item not in list_to_ignore]
         type_name[1] = type_name[1].strip('TMWIN.(')  # This is to remove schema name prefix
@@ -26,14 +26,14 @@ def get_custom_code():
     loops through schema file and creates individual files for every table, trigger, procedure etc.
     :return: None.
     """
-    code = re.split(r"@\n", sql_file.read())
+    code = re.split(r"@\n", sql_file.read())  # splits file on terminator '@'
     counter = 0
     for i in code:
         if len(i) > 1:
             title = get_code_title(i.strip())
             code_file = open(str(title+'.sql'), 'w')
             code_file.write(i.strip())
-            code_file.write('\n@\n')
+            code_file.write('\n@\n')  # re-add the query terminator that was removed
             code_file.close()
             counter += 1
     print(counter, 'files created')
