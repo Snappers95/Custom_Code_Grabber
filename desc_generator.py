7681820c9932f6a7f752d9e6bac64f173f23a15b
Snappers95 <laviolettejacob@gmail.com>
2024-03-22 10:37:23 -0400

    docstring added

--- desc_generator.py
+++ desc_generator.py
@@ -8,11 +8,16 @@ OPENAI_API_KEY = '<API_KEY>'
 
 
 def get_ai_description(prompt):
+    """
+    Use OpenAI API to get a description for each file, Passes entire SQL text to API.
+    :param prompt: The SQL text we want to describe.
+    :return: Ai-Generated Response wrapped in comment block.
+    """
     client = OpenAI(api_key=OPENAI_API_KEY)
     message = client.chat.completions.create(
         model="gpt-4-turbo-preview",
         messages=[
-            {"role": "system", "content": "Povide extremely short response describing the base functions of the provided SQL."},
+            {"role": "system", "content": "Provide extremely short response describing the base functions of the provided SQL."},
             {"role": "user", "content": prompt}
         ],
     )
